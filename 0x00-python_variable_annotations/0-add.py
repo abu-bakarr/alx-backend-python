4dd9dc23c18b9a6586e610496c7ed951b48a83c4
abu-bakarr <abangura14@yahoo.com>
2022-04-14 09:48:39 +0000

    Add exer 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,5 +5,8 @@
 
 
 def add(a: float, b: float) -> float:
-    """This Function accepts two float arguments, (a & b) and then returns the sum in a float data type"""
+    '''
+       This Function accepts two float arguments, (a & b) 
+       and then returns the sum in a float data type
+    '''
     return a + b
