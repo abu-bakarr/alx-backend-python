ccdb5900d322319c85a19fa8c40388a7f20cb6e1
abu-bakarr <abangura14@yahoo.com>
2022-04-14 09:59:00 +0000

    Add exer 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+    Sum list
+'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+        This Function accepts a list in a float data type 
+        and then returns the sum of the input list argument
+    '''
+
+    return sum(input_list)
