5beb2bcec69b368fbacd1ed31865efc63bed8e0f
abu-bakarr <abangura14@yahoo.com>
2022-04-14 10:34:57 +0000

    fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-'''
-    - add func (Annotation)
-'''
+"""
+    add 
+"""
 
 
 def add(a: float, b: float) -> float:
-    '''
-       This Function accepts two float arguments, (a & b) 
-       and then returns the sum in a float data type
-    '''
+    """This Function adds two input a and b then  returns a float of the sum"""
     return a + b
