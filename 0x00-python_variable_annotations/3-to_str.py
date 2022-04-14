7a699b0af9ca5b19d837f827ee27a776c955a0d3
abu-bakarr <abangura14@yahoo.com>
2022-04-14 09:53:36 +0000

    Add exer 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+    to string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+        This Function that accepts a float input argment
+        and then returns the string representation of that input argument
+    '''
+    return str(n)
