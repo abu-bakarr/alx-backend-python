0420362232eb81e4e3aa974459d85a0042ddac87
abu-bakarr <abangura14@yahoo.com>
2022-04-14 09:50:48 +0000

    Add exer 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+    floor
+'''
+
+import math
+
+
+def floor(n: float) -> int:
+    '''
+        This Function return the floor of the input argument
+        and then returns the floor of input argument
+    '''
+    return math.floor(n)
