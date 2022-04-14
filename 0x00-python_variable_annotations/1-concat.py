817931cd589637ca387ed96abaef10fb8fff595e
abu-bakarr <abangura14@yahoo.com>
2022-04-14 09:47:21 +0000

    Add exer 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+    concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        This Function that concat two arguments.
+        and then Returns a Concatenated string
+    '''
+    return str1 + str2
