976697cfe59deb4db414adf5c8aec39e37fe4346
abu-bakarr <abangura14@yahoo.com>
2022-04-19 09:55:48 +0000

    Add exer 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+ Run time for four parallel comprehensions
+'''
+
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''
+    this will run four time
+    '''
+    start = time.time()
+
+    await asyncio.gather(*[async_comprehension() for n in range(4)])
+
+    end = time.time()
+
+    return end - start
