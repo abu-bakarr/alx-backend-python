032ad952e735a5dc25fd9bc675f7d4a2da3558f6
abu-bakarr <abangura14@yahoo.com>
2022-05-09 12:57:34 +0000

    Add test_utils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Unit test for utils.
+"""
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Unit test CLass for access_nested_map.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nest_map, path, res):
+        """
+        Test that the method returns what it is supposed to.
+        """
+        self.assertEqual(access_nested_map(nest_map, path), res)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nest_map, path, res):
+        """
+        Test that the method returns what it is supposed to.
+        """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nest_map, path)
+        self.assertEqual(error.exception.args[0], res)
